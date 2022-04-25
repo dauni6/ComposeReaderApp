8530492bc0de5d7ef9d0295a2f2f445d23ea8219
dauni6 <45280927+dauni6@users.noreply.github.com>
2022-04-25 22:59:03 +0900

    스플래시 액티비티 만들고 OutliendInputField 작업중

---
+++ app/src/main/java/com/dontsu/composereaderapp/components/Components.kt
@@ -0,0 +1,88 @@
+package com.dontsu.composereaderapp.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ReaderLogoText(modifier: Modifier = Modifier) {
+    Text(
+        modifier = modifier
+            .padding(bottom = 16.dp),
+        text = "A. Reader",
+        style = MaterialTheme.typography.h3,
+        color = Color.Red.copy(alpha = 0.5f)
+    )
+
+}
+
+@Composable
+fun EmailInput(
+    modifier: Modifier = Modifier,
+    emailState: MutableState<String>,
+    labelId: String = "Email",
+    enabled: Boolean = true,
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardActions: KeyboardActions = KeyboardActions.Default
+) {
+    InputField(
+        modifier = modifier,
+        valueState = emailState,
+        labelId = labelId,
+        enabled = enabled,
+        imeAction = imeAction,
+        keyboardActions = keyboardActions
+    )
+}
+
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    labelId: String,
+    enabled: Boolean,
+    isSingleLine: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Next,
+    keyboardActions: KeyboardActions = KeyboardActions.Default
+) {
+    OutlinedTextField(
+        modifier = modifier
+            .padding(bottom = 10.dp, start = 10.dp, end = 10.dp)
+            .fillMaxWidth(),
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
+        label = { Text(text = labelId) },
+        singleLine = isSingleLine,
+        enabled = enabled,
+        textStyle = TextStyle(fontSize = 18.sp, color = MaterialTheme.colors.onBackground),
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+        keyboardActions = keyboardActions
+    )
+}
--- app/src/main/java/com/dontsu/composereaderapp/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/dontsu/composereaderapp/screens/login/ReaderLoginScreen.kt
@@ -1,9 +1,80 @@
 package com.dontsu.composereaderapp.screens.login
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.dontsu.composereaderapp.components.EmailInput
+import com.dontsu.composereaderapp.components.ReaderLogoText
 
 @Composable
 fun ReaderLoginScreen(navController: NavHostController) {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ReaderLogoText()
+        }
+    }
+
+}
+
+@ExperimentalComposeUiApi
+@Preview
+@Composable
+fun UserForm() {
+    val email = rememberSaveable { mutableStateOf("") }
+    val password = rememberSaveable { mutableStateOf("") }
+    val passwordVisibility = rememberSaveable { mutableStateOf(false) }
+    val passwordFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+
+    val modifier = Modifier
+        .height(250.dp)
+        .background(MaterialTheme.colors.background)
+        .verticalScroll(rememberScrollState())
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        EmailInput(
+            emailState = email,
+            enabled = true,
+            keyboardActions = KeyboardActions {
+                passwordFocusRequest.requestFocus() // email을 다 적으면 password field에 포커스 가도록 하기
+            }
+        )
+
+//        PasswordInput(
+//            modifier = Modifier.focusRequester(passwordFocusRequest)
+//        )
+    }
 
 }
